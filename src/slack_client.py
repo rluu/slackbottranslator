17b5710372c95254a9231b1c300b29cba56cc33d
Potluru, Vamsi <vamsi_potluru@cable.comcast.com>
2016-04-26 20:30:31 -0400

    fix dummy print and also added real-time messaging

--- src/slack_client.py
+++ src/slack_client.py
@@ -2,7 +2,11 @@ import time
 from slackclient import SlackClient
 token='xoxp-19745829781-38048263175-38056115377-d6ad0242f9'
 sc = SlackClient(token)
-print sc.api_call("api.test")
-sc.api_call?
 print sc.api_call("chat.postMessage", channel="#group3", text="Hello from slack client", username="ismav29", icon_emoji=':robot_face:')
 
+if sc.rtm_connect():
+    while True:
+        print sc.rtm_read()
+        time.sleep(1)
+else:
+    print "Connection Failed, invalid token?"
